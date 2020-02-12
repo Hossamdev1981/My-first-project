b30fe68610baab04117af9e4023f69c05093ba78
unknown <Hossam.alaidy@hotmail.com>
2020-02-12 11:59:08 -0700

    c sharp

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace c_review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //    int x = 30;
+        //     //Console.WriteLine("Hello Hossam!"+x);
+        //     if(x<30)
+        //     {
+        //     Console.WriteLine("x is Less than thirty");
+        //     } 
+        //     else if (x==30) {       
+                
+        //              Console.WriteLine("x is thirty!");
+        //              }
+
+        //     else {
+        //     Console.WriteLine("x is greater than thirty");}
+
+        int a = 3;
+        switch (a) {
+            case 1 :
+            Console.WriteLine( "value is 1!");
+            break;
+            case 2 :
+            Console.WriteLine( "value is 2!");
+            break;
+             case 3 :
+            Console.WriteLine( "value is 3!");
+            break;
+            default:
+            break;
+        }
+
+
+
+            }
+        }
+    }
+
